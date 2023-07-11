fd24026d12deee3999eb980997799e6756aed463
Leng Tan <letng27@gmail.com>
2023-07-11 00:02:38 -0400

    Add function playRound()
    Return "draw" if player draws with the computer, "lose" if the player loses, and "win" if the player wins

--- script.js
+++ script.js
@@ -6,3 +6,33 @@ function getComputerChoice(){//returns rock paper or scissors
      */
     return (Math.floor(Math.random() * (3)));//outputs 0, 1 , or 2
 }
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return("draw")
+    }
+    else if(playerSelection == 0){//player choose rock
+        if(computerSelection == 1){//CPU plays paper
+            return("lose");
+        }
+        if(computerSelection == 2){//CPU plays scissors
+            return("win");
+        }
+    }
+    else if(playerSelection == 1){//player choose paper
+        if(computerSelection == 0){//CPU plays rock
+            return("win");
+        }
+        if(computerSelection == 2){//CPU plays scissors
+            return("lose");
+        }
+    }
+    else if(playerSelection == 2){//player choose scissors
+        if(computerSelection == 0){//CPU plays rock
+            return("lose");
+        }
+        if(computerSelection == 1){//CPU plays paper
+            return("win");
+        }
+    }
+}

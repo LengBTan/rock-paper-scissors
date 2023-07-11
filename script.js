2d8ced71e43f47cd1c7b86995856a5a2832f743d
Leng Tan <letng27@gmail.com>
2023-07-10 23:34:09 -0400

    Add function to determine the computer's choice

---
+++ script.js
@@ -0,0 +1,8 @@
+function getComputerChoice(){//returns rock paper or scissors
+    /**
+     * 0 is rock
+     * 1 is paper
+     * 2 is scissors
+     */
+    return (Math.floor(Math.random() * (3)));//outputs 0, 1 , or 2
+}
